1a755ab97182d1211725523df668d3e2cc5eedfb
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-29 19:51:26 -0300

    Create reducer for genres

--- src/redux/genres/genres.js
+++ src/redux/genres/genres.js
@@ -1,9 +1,20 @@
-import GenresList from "../../components/genres-list";
-
-const Genres = () => {
-  return (
-    <GenresList />
-  );
+const initialState = {
+  genres: [
+    { name: 'rock', key: 1 },
+    { name: 'pop', key: 2 },
+    { name: 'hip hop', key: 3 },
+    { name: 'rap', key: 4 },
+    { name: 'metal', key: 5 },
+    { name: 'electronic', key: 6 },
+    { name: 'folk', key: 7 },
+  ]
 }
- 
-export default Genres;
+
+const genresReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default genresReducer;
