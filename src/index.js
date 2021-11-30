935b3a152c01493b7c6a44089c1f88e9f64908e6
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-30 13:05:48 -0300

    Import index.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
