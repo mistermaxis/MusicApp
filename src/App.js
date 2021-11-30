05a3a7d825a8e4fe60975c4a7417fdae6b156fe5
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-30 13:05:01 -0300

    Import App.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Albums from './pages/albums';
 import Genres from './pages/genres';
 import Songs from './pages/songs';
+import './App.css';
 
 function App() {
   return (
