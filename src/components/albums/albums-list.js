ef9b844e0bacb191b254a346a27fddc2ee8c4011
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-30 18:55:19 -0300

    Add search input

--- src/components/albums/albums-list.js
+++ src/components/albums/albums-list.js
@@ -2,6 +2,12 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Stack from 'react-bootstrap/Stack';
+import Form from 'react-bootstrap/Form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { fetchAlbums } from '../../redux/albums/albums';
 
 const AlbumsList = () => {
@@ -14,21 +20,29 @@ const AlbumsList = () => {
   }, [dispatch, genre]);
 
   return (
-    <div>
+    <Container>
+      <Stack direction="horizontal" gap={3}>
+        <Link to="/" className="me-auto">
+          <FontAwesomeIcon icon={faCoffee} />
+        </Link>
+        <Form.Control className="rounded" type="text" placeholder="Album" />
+      </Stack>
       <h3>{genre.toUpperCase()}</h3>
-      {
-        albums.map((album) => {
-          if (album.mbid.length !== 0) {
-            return (
-              <p key={album.mbid}>
-                <Link to={`/album/${album.mbid}`}>{album.name}</Link>
-              </p>
-            );
-          }
-          return null;
-        })
-      }
-    </div>
+      <ListGroup>
+        {
+          albums.map((album) => {
+            if (album.mbid.length !== 0) {
+              return (
+                <ListGroup.Item as={Link} to={`/album/${album.mbid}`} key={album.mbid}>
+                  {album.name}
+                </ListGroup.Item>
+              );
+            }
+            return null;
+          })
+        }
+      </ListGroup>
+    </Container>
   );
 };
 
