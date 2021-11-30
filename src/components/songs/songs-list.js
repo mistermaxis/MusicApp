842fc67fd097294090b8b107895c47cd1a7cb00f
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-30 13:04:21 -0300

    Add external function to render songs

--- src/components/songs/songs-list.js
+++ src/components/songs/songs-list.js
@@ -1,34 +1,37 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
-import { fetchSongs } from "../../redux/songs/songs";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { fetchSongs } from '../../redux/songs/songs';
 
 const SongsList = () => {
-  const { album_id } = useParams();
-  const album = useSelector(state => state.albumReducer.album);
+  const { album_id: albumId } = useParams();
+  const album = useSelector((state) => state.albumReducer.album);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchSongs(album_id));
-  }, [dispatch, album_id]);
+    dispatch(fetchSongs(albumId));
+  }, [dispatch, albumId]);
+
+  function renderSongs() {
+    if (album.album) {
+      return album.album.tracks.track.map((song) => (
+        <p key={song['@attr'].rank}>{song.name}</p>
+      ));
+    }
+    if (album.error) {
+      return <p>{album.error}</p>;
+    }
+    return <p>Loading</p>;
+  }
 
   return (
     <div>
       {
-        album.album ? (
-          album.album.tracks.track.map((song) => {
-            return (
-              <p key={song['@attr'].rank}>
-                {song.name}
-              </p>
-            )
-          })
-        ) : (
-          <div>Hello</div>
-        )
+        album.album ? <h1>{album.album.name}</h1> : null
       }
+      {renderSongs()}
     </div>
   );
-}
+};
 
 export default SongsList;
