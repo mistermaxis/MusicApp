7c880e3a251a5876be86b94b3a70f2f49a47d390
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-30 18:54:50 -0300

    Import bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render(
