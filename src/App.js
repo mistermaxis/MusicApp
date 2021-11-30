66caa0ded133ee6d0b08e2aa8d04e42ca3587cfc
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-30 02:16:49 -0300

    Add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Albums from './pages/albums';
 import Genres from './pages/genres';
+import Songs from './pages/songs';
 
 function App() {
   return (
-    <div className="App">
-      <Genres />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Genres />} />
+          <Route path="/genre/:genre_id" element={<Albums />} />
+          <Route path="/album/:album_id" element={<Songs />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
